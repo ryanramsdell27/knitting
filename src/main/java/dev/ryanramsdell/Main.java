3f6eaed3b2b54f94b6bcca3e2542b54b6ab35f11
Ryan <ryan.ramsdell.27@gmail.com>
2023-04-20 16:57:55 -0500

    Decreases in language

--- src/main/java/dev/ryanramsdell/Main.java
+++ src/main/java/dev/ryanramsdell/Main.java
@@ -9,39 +9,12 @@ import java.io.StringReader;
 
 public class Main {
     public static void main(String[] args) {
-        KnittingPattern pattern = new KnittingPattern(64);
-        pattern.knit(128);
-        for(int i = 0; i < 4; i++) {
-            pattern.k2tog();
-            pattern.knit(14);
-        }
-        pattern.knit(120);
-        for(int i = 0; i < 6; i++) {
-            pattern.k2tog();
-            pattern.knit(8);
-        }
-        pattern.knit(108);
-        for(int i = 0; i < 6; i++) {
-            pattern.k2tog();
-            pattern.knit(7);
-        }
-        pattern.knit(48);
-        for(int i = 0; i < 8; i++) {
-            pattern.k2tog();
-            pattern.knit(4);
-        }
-        pattern.knit(40);
-        for(int i = 0; i < 10; i++) {
-            pattern.k2tog();
-            pattern.knit(2);
-        }
-        pattern.knit(30);
-        System.out.println(Blender.toBlenderApi(pattern));
-
-        Reader reader = new StringReader("co5k5p3 knit2 p2k2pkpk");
+        KnittingPattern pattern;
+        Reader reader = new StringReader("co64k256p512 knit2 p2k2pkpkk2tog");
         Knit knit = new Knit(reader);
         try{
-            knit.Start();
+            pattern = knit.Start();
+            System.out.println(Blender.toBlenderApi(pattern));
         } catch (Exception e) {
             System.out.println(e);
         }
